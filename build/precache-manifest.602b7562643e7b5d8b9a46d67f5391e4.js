30923b865d4c87c7b37b6ebd3f4c681cc106559c
Grzegorz Głuchowski <ggrzegorz89@gmail.com>
2022-02-15 21:21:57 +0100

    Build

---
+++ build/precache-manifest.602b7562643e7b5d8b9a46d67f5391e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1d06d6ca6c30f057748279e9fa1a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4718bc27d3f82d033d02",
+    "url": "/static/css/main.9d096a0b.chunk.css"
+  },
+  {
+    "revision": "2d47e1a5934491b9f7a1",
+    "url": "/static/js/2.c0add85b.chunk.js"
+  },
+  {
+    "revision": "4718bc27d3f82d033d02",
+    "url": "/static/js/main.13c1af3d.chunk.js"
+  },
+  {
+    "revision": "b7d7c5072affad8d663f",
+    "url": "/static/js/runtime-main.70f65feb.js"
+  }
+]);
